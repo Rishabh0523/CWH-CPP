59b92af83801dfc2f5cd841f19e1fd340b3d7e72
Rishabh0523 <rishabhsingh.rs432@gmail.com>
2022-01-28 17:56:23 +0530

    CPP

---
+++ Lec3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*   MULTILINE COMMENTS :-
+    Lala           
+    Jack
+    Vishal
+    Chiura
+    Madho
+    Dugu
+    chintu
+    ye sab chutiya h */ 
+    cout<<"Rishabh Singh"<<endl;
+    //Sinlge Line comments :- 
+   //Ramnagar is my home Town 
+    return 0; 
+}
